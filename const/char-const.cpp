182eaa743f638970782caea3c02749f1a0d34dac
Peterhabit <jiseokchoij@gmail.com>
2022-11-04 13:38:26 +0900

    feat: const char*에 관한 소스

---
+++ const/char-const.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using std::cout;
+class Cat {
+private: //생략가능
+	int age;
+	char name[20];
+	// const char* name; //A
+public:
+	Cat(int age, const char* n) {
+		this->age = age;
+		strcpy(name, n); // name=n; //A
+		cout << name << "고양이 객체가 만들어졌어요.\n";
+	}
+	~Cat() { cout << name << "객체 바이\n"; };
+	int getAge();
+	const char* getName();
+	void setAge(int age);
+	void setName(const char* pName);
+	void meow();
+};
+int Cat::getAge() {
+	return age;
+}
+void Cat::setAge(int age) {
+	this->age = age;
+}
+void Cat::setName(const char* pName) {
+	strcpy(name, pName);
+	//strcpy(대상주소, 원본주소);
+	//strcpy_s(대상주소, 대상의길이, 원본주소);
+	//name=pName; //A
+}
+const char* Cat::getName() {
+	return name;
+}
+void Cat::meow() {
+	cout << name << "고양이가 울어요\n";
+}
+int main() {
+	Cat nabi(1, "나비"), yaong(1, "야옹"), * pNabi;
+	cout << nabi.getName() << " 출생 나이는 " << nabi.getAge() << "살이다.\n";
+	cout << yaong.getName() << " 출생 나이는 " << yaong.getAge() << "살이다.\n";
+	pNabi = &nabi;
+	cout << pNabi->getName() << " 출생 나이는 " << pNabi->getAge() << "살이다.\n";
+	nabi.setName("Nabi");
+	nabi.setAge(3);
+	cout << nabi.getName() << " 나이는 " << nabi.getAge() << "살이다.\n";
+	yaong.meow();
+	nabi.meow();
+	return 0;
+}
+//나비고양이 객체가 만들어졌어요.
+//야옹고양이 객체가 만들어졌어요.
+//나비 출생 나이는 1살이다.
+//야옹 출생 나이는 1살이다.
+//나비 출생 나이는 1살이다.
+//Nabi 나이는 3살이다.
+//야옹고양이가 울어요
+//Nabi고양이가 울어요
+//야옹객체 바이
+//Nabi객체 바이v
